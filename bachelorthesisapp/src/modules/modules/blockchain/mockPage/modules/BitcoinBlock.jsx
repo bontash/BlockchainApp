e827e140be57b711af120ab472edc3c35cd15859
Lorena Bontaș <bontas.lorena@yahoo.com>
2022-08-12 00:52:39 +0300

    added mock bitcoin blockchain functionalities - done

--- bachelorthesisapp/src/modules/modules/blockchain/mockPage/modules/BitcoinBlock.jsx
+++ bachelorthesisapp/src/modules/modules/blockchain/mockPage/modules/BitcoinBlock.jsx
@@ -1,51 +1,29 @@
-import React, {useEffect, useState} from "react";
-import {Button, Card, CardContent, Grid} from "@mui/material";
+import React from "react";
+import {Card, CardContent, Grid} from "@mui/material";
 import BlockField from "../components/BlockField";
-import NodeButton from "../components/NodeButton";
+import ClearIcon from '@mui/icons-material/Clear';
+import CheckIcon from '@mui/icons-material/Check';
 
-const BitcoinBlock = ({blockInfo, setBlockInfo}) => {
-
-    const [blockHash, setBlockHash] = useState("");
-
-    const fetchHash = (blockInfo, setResponse) => {
-
-        const requestOptions = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                blockNr: blockInfo.blockNr,
-                nonce: blockInfo.nonce,
-                timestamp: blockInfo.timestamp,
-                value: blockInfo.value,
-                prevBlockHash: blockInfo.prevBlockHash,
-                hash: blockInfo.hash
-            })
-        };
-        fetch('http://localhost:3001/bitcoinEncryption', requestOptions)
-            .then(response => response.json())
-            .then(data => setResponse(data.finalHash));
-    }
-
-    useEffect(() => {
-        setBlockInfo(blockHash,"hash");
-    },[blockHash])
-
+const BitcoinBlock = ({blockInfo, setBlockInfo, isCorrupted}) => {
 
     return <Grid container direction="row" justifyContent="space-evenly" alignItems="flex-start">
-        <Card sx={{width: 300, height: 485, backgroundColor: 'lightgoldenrodyellow'}}>
+        <Card sx={{width: 300, height: 520, backgroundColor: 'lightgoldenrodyellow'}}>
             <CardContent>
                 <BlockField fieldName={"Block nr"} fieldValue={blockInfo.blockNr}
-                            onChange={(e) => setBlockInfo(e.target.value, "blockNr")} readOnly/>
+                            onChange={async (e) => await setBlockInfo(e.target.value, "blockNr")} readOnly/>
                 <BlockField fieldName={"Nonce"} fieldValue={blockInfo.nonce}
-                            onChange={(e) => setBlockInfo(e.target.value, "nonce")}/>
+                            onChange={async (e) => await setBlockInfo(e.target.value, "nonce")}/>
                 <BlockField fieldName={"Timestamp"} fieldValue={blockInfo.timestamp}
-                            onChange={(e) => setBlockInfo(e.target.value, "timestamp")} readOnly/>
+                            onChange={async (e) => await setBlockInfo(e.target.value, "timestamp")} readOnly/>
                 <BlockField fieldName={"Value"} fieldValue={blockInfo.value}
-                            onChange={(e) => setBlockInfo(e.target.value, "value")}/>
+                            onChange={async (e) => await setBlockInfo(e.target.value, "value")}/>
                 <BlockField fieldName={"Previous block hash"} fieldValue={blockInfo.prevBlockHash}
-                            onChange={(e) => setBlockInfo(e.target.value, "prevBlockHash")} readOnly/>
-                <BlockField fieldName={"Hash"} fieldValue={blockHash} onChange={(e) => {setBlockInfo(e.target.value, "hash");console.log(e)}}/>
-                <NodeButton onClick={() => fetchHash(blockInfo, setBlockHash)}/>
+                            onChange={async (e) => await setBlockInfo(e.target.value, "prevBlockHash")} readOnly/>
+                <BlockField fieldName={"Hash"} fieldValue={blockInfo.hash} readOnly/>
+                {
+                    isCorrupted ? <Grid container alignItems={"center"}><Grid item><ClearIcon/></Grid></Grid> :
+                        <Grid container alignItems={"center"}><Grid item><CheckIcon/></Grid></Grid>
+                }
             </CardContent>
         </Card>
     </Grid>
--- bachelorthesisapp/src/modules/modules/blockchain/mockPage/modules/BitcoinBlockchain.jsx
+++ bachelorthesisapp/src/modules/modules/blockchain/mockPage/modules/BitcoinBlockchain.jsx
@@ -1,47 +1,102 @@
 import {Button, Grid} from "@mui/material";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import BitcoinBlock from "./BitcoinBlock";
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import {CustomButton} from "../../../../core/ui/CustomButton";
 
-const BitcoinBlockchain = () => {
-    const [blocks, setBlocks] = useState([{
-        blockNr: 1,
-        nonce: 0,
-        timestamp: Date.now().toString(),
-        value: "",
-        prevBlockHash: "000000000000000000000000000000000000000",
-        hash: ""
-    }]);
+const defaultBlock = {
+    blockNr: 1,
+    nonce: 0,
+    timestamp: Date.now().toString(),
+    value: "",
+    prevBlockHash: "000000000000000000000000000000000000000",
+    hash: ""
+}
 
-    function addBlocks() {
-        const newBlockchain = [...blocks];
-        newBlockchain.push({
-            blockNr: blocks[blocks.length - 1].blockNr + 1,
-            nonce: 0,
-            timestamp: Date.now().toString(),
-            value: "",
-            prevBlockHash: blocks[blocks.length - 1].hash,
-            hash: ""
+async function fetchHash(blockInfo) {
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            blockNr: blockInfo.blockNr,
+            nonce: blockInfo.nonce,
+            timestamp: blockInfo.timestamp,
+            value: blockInfo.value,
+            prevBlockHash: blockInfo.prevBlockHash,
+            hash: blockInfo.hash
         })
+    };
+
+    return await fetch('http://localhost:3001/bitcoinEncryption', requestOptions).then(data => data.json())
+}
+
+const BitcoinBlockchain = () => {
+    const [blocks, setBlocks] = useState([]);
+    const [corruptedArray, setCorruptedArray] = useState([]);
+
+    async function addBlocks() {
+        const newBlockchain = [...blocks];
+        const newBlock = {
+            ...defaultBlock,
+            timestamp: Date.now().toString(),
+            blockNr: blocks[blocks.length - 1].blockNr + 1,
+            prevBlockHash: blocks[blocks.length - 1].hash,
+        }
+        const {finalHash, error} = await fetchHash(newBlock);
+        newBlockchain.push({...newBlock, hash: finalHash});
         setBlocks(newBlockchain);
+        setCorruptedArray([...corruptedArray, false]);
     }
 
-    function checkHash() {
-        let isCorrupted = false;
-        for (let i = 1; i < blocks.length; i++) {
-            if (blocks[i - 1].hash !== blocks[i].hash)
-                isCorrupted = true;
+    useEffect(() => {
+        async function fetchInitialData() {
+            const {finalHash, error} = await fetchHash(defaultBlock);
+            setBlocks([{...defaultBlock, hash: finalHash}])
+            setCorruptedArray([false])
         }
-        return isCorrupted;
-    }
+
+        fetchInitialData();
+    }, [])
+
+    useEffect(()=>{
+        let index;
+        if(blocks.length !== 1){
+            const newCorruptedArray = [...corruptedArray];
+            for (let i = 1; i < blocks.length - 1; i++) {
+                if (blocks[i].hash !== blocks[i + 1].prevBlockHash)
+                {
+                    index = i + 1;
+                    break;
+                }
+                else newCorruptedArray[i + 1] = false;
+            }
+            for (let j = index; j < blocks.length; j++) {
+                newCorruptedArray[j] = true;
+            }
+            setCorruptedArray(newCorruptedArray);
+        }
+    },[blocks])
 
     return <Grid container direction="row" justifyContent={"flex-start"} alignItems={"center"}>
         {blocks.map((block, idx) => {
             return (<Grid item xs={3} key={"btc-block" + idx}>
-                <BitcoinBlock blockInfo={block}
-                              setBlockInfo={(value, field) => setBlocks(prevValue => prevValue.map((prevBlock, prevBlockIdx) =>
-                                  prevBlockIdx !== idx ? prevBlock : {...prevBlock, [field]: value}))}/>
+                <BitcoinBlock blockInfo={block} isCorrupted={corruptedArray[idx]}
+                              setBlockInfo={async (value, field) => {
+                                  const {finalHash, error} = await fetchHash({...block, [field]: value});
+                                  setBlocks(prevValue => {
+                                      return prevValue.map((prevBlock, prevBlockIdx) =>
+                                          prevBlockIdx !== idx
+                                                  ? prevBlock
+                                                  : {
+                                                      ...prevBlock,
+                                                      hash: finalHash,
+                                                      [field]: value
+                                                  }
+                                      )
+                                  })
+                              }}
+                />
             </Grid>)
         })}
         {blocks.length < 4 && <Grid item xs={3} alignContent={"center"}>
