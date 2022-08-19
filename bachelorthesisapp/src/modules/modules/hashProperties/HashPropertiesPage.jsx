43862867d6d89bef75739852da37c8b1b4453cbc
Lorena Bontaș <bontas.lorena@yahoo.com>
2022-08-19 21:06:15 +0300

    added message for snackbar

--- bachelorthesisapp/src/modules/modules/hashProperties/HashPropertiesPage.jsx
+++ bachelorthesisapp/src/modules/modules/hashProperties/HashPropertiesPage.jsx
@@ -26,8 +26,9 @@ const HashProperties = () => {
         <ParticlesBackground />
         <Snackbar sx={{maxWidth: 600}} message={"For a user who wants more in-depth knowledge about properties of the various hash functions " +
         "applied in the chain, for a more educated choice of what blockchain to use, this page provides basic characteristics of those functions. A " +
-        "user can select more than one hash type, to see a clear comparison between them. The avalanche effect is a function created by me " +
-        "and the non-correlation is computed using the Damerau-Levenshtein distance."} autoHideDuration={15000} open={open} onClose={handleClose} anchorOrigin={{vertical: 'top', horizontal: 'left'}}
+        "user can select more than one hash type, to see a clear comparison between them. The avalanche effect is a criterion which states that a small " +
+        "modification in the input bits(like flipping a bit) should result in a modification of the output bits by ~50% for a good hash function. Another " +
+        "property is that the length should always be fixed and not depend on the length of the input. Finally, a good hash function should be quick and easy to compute."} autoHideDuration={20000} open={open} onClose={handleClose} anchorOrigin={{vertical: 'top', horizontal: 'left'}}
         />
             <PageTitle>Cryptographic hash functions properties</PageTitle>
             <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
@@ -45,7 +46,7 @@ const HashProperties = () => {
             <Button variant={'contained'} onClick={() => {
                 fetchEncryptedString(hashList, stringToEncrypt, setEncryptionResponse)
             }}>
-                Hash string
+                Hash data
             </Button>
             <p></p>
             </Box>
