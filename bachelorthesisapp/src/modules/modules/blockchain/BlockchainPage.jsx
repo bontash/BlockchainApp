580b9048732521536b52b9c856479b596f42406b
Lorena Bontaș <bontas.lorena@yahoo.com>
2022-08-19 12:32:57 +0300

    added history for blockchain and transactions pages

--- bachelorthesisapp/src/modules/modules/blockchain/BlockchainPage.jsx
+++ bachelorthesisapp/src/modules/modules/blockchain/BlockchainPage.jsx
@@ -31,7 +31,8 @@ const BlockchainPage = () => {
                 <Typography fontSize={'115%'}>
                     Blockchain technology encompasses a series of functionalities to ensure data protection, at the same
                     time
-                    allowing a semi-transparency to exist. Its core mechanisms are the ones presented on the cards below. Hover over them to go
+                    allowing a semi-transparency to exist. Its core mechanisms are the ones presented on the cards
+                    below. Hover over them to go
                     to the page where the functionality is presented.
                 </Typography>
             </BlockchainPageMainSection>
@@ -39,14 +40,16 @@ const BlockchainPage = () => {
         <p/>
         <Grid container direction={'row'} justifyContent={'space-evenly'} alignItems={'center'}>
             <Grid item alignContent={'center'}>
-                <BlockchainPageFlipCard pageName={'Mock'} description={"Protection against possible attacks, using a cryptographic " +
-                "link between each node. After a node is mined, you cannot change its content. The link is a hash function, which updates when a change occurs in a block." +
-                "All the blocks after the tampered one will be notified that the link is broken and they are corrupted."}/>
+                <BlockchainPageFlipCard onClick={() => history.push(routes[0].path)} pageName={'Mock'}
+                                        description={"Protection against possible attacks, using a cryptographic " +
+                                        "link between each node. After a node is mined, you cannot change its content. The link is a hash function, which updates when a change occurs in a block." +
+                                        "All the blocks after the tampered one will be notified that the link is broken and they are corrupted."}/>
             </Grid>
             <Grid item>
-                <BlockchainPageFlipCard pageName={'Transactions'} description={"Exchange transactions without a third-party to facilitate the process. Everything is based " +
-                "on mutual trust and transparency. The transaction is added into a mined block and you can see its details. Every transaction has a fee which will be added " +
-                "at the total cost, in order to make people be aware of their actions on the chain."}/>
+                <BlockchainPageFlipCard onClick={() => history.push(routes[1].path)} pageName={'Transactions'}
+                                        description={"Exchange transactions without a third-party to facilitate the process. Everything is based " +
+                                        "on mutual trust and transparency. The transaction is added into a mined block and you can see its details. Every transaction has a fee which will be added " +
+                                        "at the total cost, in order to make people be aware of their actions on the chain."}/>
             </Grid>
         </Grid>
     </>
--- bachelorthesisapp/src/modules/modules/blockchain/transactionsPage/TransactionsMainPage.jsx
+++ bachelorthesisapp/src/modules/modules/blockchain/transactionsPage/TransactionsMainPage.jsx
@@ -4,29 +4,33 @@ import {ParticlesBackground} from "../../../core/ui/background/ParticlesBackgrou
 import {PageTitle} from "../../../core/ui/PageTitle";
 import {TransactionsPageMainSection} from "./components/TransactionsPageMainSection";
 import TransactionsPageCard from "./components/TransactionsPageCard";
+import {useHistory} from "react-router-dom";
 
 const TransactionsMainPage = () => {
+
+    const history = useHistory();
+    const routes = ["/bitcoinTransaction", "/ethereumTransaction"]
     const logos = ["https://png.monster/wp-content/uploads/2022/02/png.monster-623.png", "https://www.pngall.com/wp-content/uploads/10/Ethereum-Classic-Logo-PNG-Pic.png"]
     return <>
-            <ParticlesBackground/>
-            <PageTitle>
-                Transactions main page
-            </PageTitle>
-            <TransactionsPageMainSection>
-                <Typography fontSize={'115%'}>
+        <ParticlesBackground/>
+        <PageTitle>
+            Transactions main page
+        </PageTitle>
+        <TransactionsPageMainSection>
+            <Typography fontSize={'115%'}>
 
-                </Typography>
-            </TransactionsPageMainSection>
-            <Grid container direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
-                <TransactionsPageCard pageName={'Bitcoin'}
-                                      description={'Connection made to the Bitcoin Testnet, which provides the same functionalities' +
-                                      ' as the Bitcoin Mainnet, with the help of bitcore-lib JS library and Bitpay wallet.'}
-                                      logo={logos[0]}/>
-                <TransactionsPageCard pageName={'Ethereum'}
-                                      description={'Connection to the Ropsten Testnet, which has the same capabilities as the Ethereum Mainnet,' +
-                                      'the following functionalities are achieved through the web3.js React library and Metamask.'}
-                                      logo={logos[1]}/>
-            < /Grid>
+            </Typography>
+        </TransactionsPageMainSection>
+        <Grid container direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
+            <TransactionsPageCard onClick={() => history.push(routes[0])} pageName={'Bitcoin'}
+                                  description={'Connection made to the Bitcoin Testnet, which provides the same functionalities' +
+                                  ' as the Bitcoin Mainnet, with the help of bitcore-lib JS library and Bitpay wallet.'}
+                                  logo={logos[0]}/>
+            <TransactionsPageCard onClick={() => history.push(routes[1])} pageName={'Ethereum'}
+                                  description={'Connection to the Ropsten Testnet, which has the same capabilities as the Ethereum Mainnet,' +
+                                  'the following functionalities are achieved through the web3.js React library and Metamask.'}
+                                  logo={logos[1]}/>
+        < /Grid>
     </>
 }
 
