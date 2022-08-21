253263609f7aa2a325d77e9a4292df2ea950b977
Lorena Bontaș <bontas.lorena@yahoo.com>
2022-08-21 14:15:20 +0300

    finished mock page

--- bachelorthesisapp/src/modules/modules/blockchain/mockPage/MockPage.jsx
+++ bachelorthesisapp/src/modules/modules/blockchain/mockPage/MockPage.jsx
@@ -21,8 +21,11 @@ const MockPage = () => {
 
     return <>
         <ParticlesBackground/>
-        <Snackbar sx={{maxWidth: 600}} message={"This web-application aims to provide the user a deeper understanding of how cryptography\n" +
-        "                and blockchain are related. At the core, there are 3 cryptographic hash functions, presented below."} autoHideDuration={15000} open={open} onClose={handleClose} anchorOrigin={{vertical: 'top', horizontal: 'left'}}
+        <Snackbar sx={{maxWidth: 800}} message={"Here you can see several aspects of hash functions and blockchains. For the hashes, you can see that " +
+        " it changes when each input changes and if you remove the changes, the hash is the same as before. For blockchains, you can see some of the fields which take " +
+        "part in the construction of the node, you can see the link between them using hashes and if you wish to change some field in a mined block, all the nodes after it will" +
+        "be notified that something is wrong. The first block of each chain is called the genesis block and has the hash = 0. Here I already generated the first random hash after " +
+        "the genesis one."} autoHideDuration={60000} open={open} onClose={handleClose} anchorOrigin={{vertical: 'top', horizontal: 'left'}}
         />
             <PageTitle>
                 Mock blockchains
