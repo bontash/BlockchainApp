6f17d507f0ebee270355e025479c8d98b5db3be7
Lorena Bontaș <bontas.lorena@yahoo.com>
2022-08-21 14:26:48 +0300

    finished blockchain main page

--- bachelorthesisapp/src/modules/modules/blockchain/BlockchainPage.jsx
+++ bachelorthesisapp/src/modules/modules/blockchain/BlockchainPage.jsx
@@ -41,15 +41,21 @@ const BlockchainPage = () => {
         <Grid container direction={'row'} justifyContent={'space-evenly'} alignItems={'center'}>
             <Grid item alignContent={'center'}>
                 <BlockchainPageFlipCard onClick={() => history.push(routes[0].path)} pageName={'Mock'}
-                                        description={"Protection against possible attacks, using a cryptographic " +
-                                        "link between each node. After a node is mined, you cannot change its content. The link is a hash function, which updates when a change occurs in a block." +
-                                        "All the blocks after the tampered one will be notified that the link is broken and they are corrupted."}/>
+                                        descriptionFront={"Protection against possible attacks, using a cryptographic " +
+                                        "link between each node. After a node is mined, you cannot change its content. The link is a hash, which updates when a change occurs in a block." +
+                                        "All the blocks after the tampered one will be notified that the link is broken and they are corrupted."}
+                                        descriptionBack={"Choose from " +
+                                        "2 mock blockchains, which mimic the interaction with the real ones. Here, the data can be any string you wish. In a real life scenario, " +
+                                        "presented in the next flip card, data can be only positive integers."}/>
             </Grid>
             <Grid item>
                 <BlockchainPageFlipCard onClick={() => history.push(routes[1].path)} pageName={'Transactions'}
-                                        description={"Exchange transactions without a third-party to facilitate the process. Everything is based " +
+                                        descriptionFront={"Exchange transactions without a third-party to facilitate the process. Everything is based " +
                                         "on mutual trust and transparency. The transaction is added into a mined block and you can see its details. Every transaction has a fee which will be added " +
-                                        "at the total cost, in order to make people be aware of their actions on the chain."}/>
+                                        "at the total cost, in order to make people be aware of their actions on the chain."}
+                                        descriptionBack={"Choose " +
+                                        "which blockchain you wish to see doing a real-time transaction. You need to have installed Metamask web extension for interaction " +
+                                        "with Ethereum and any wallet which supports the Bitcoin Testnet, like Bitpay. "}/>
             </Grid>
         </Grid>
     </>
--- bachelorthesisapp/src/modules/modules/blockchain/modules/BlockchainPageFlipCard.jsx
+++ bachelorthesisapp/src/modules/modules/blockchain/modules/BlockchainPageFlipCard.jsx
@@ -9,14 +9,15 @@ import {CustomButton} from "../../../core/ui/CustomButton";
 import {Button, Grid, Typography} from "@mui/material";
 import {BlockchainFlipCardText} from "../transactionsPage/components/BlockchainFlipCardText";
 
-const BlockchainPageFlipCard = ({onClick, pageName, description}) => {
+const BlockchainPageFlipCard = ({onClick, pageName, descriptionFront, descriptionBack}) => {
     return <Grid item xl={3} m={6}>
         <FlipCard>
             <FlipCardInner>
                 <FlipCardFront>
-                        <BlockchainFlipCardText alignContent={'center'}>{description}</BlockchainFlipCardText>
+                        <BlockchainFlipCardText alignContent={'center'}>{descriptionFront}</BlockchainFlipCardText>
                 </FlipCardFront>
                 <FlipCardBack>
+                    <BlockchainFlipCardText alignContent={'center'}>{descriptionBack}</BlockchainFlipCardText>
                     <CustomButton>
                         <Button color="secondary" variant="contained" onClick={onClick}>
                             Go to {pageName} page->
